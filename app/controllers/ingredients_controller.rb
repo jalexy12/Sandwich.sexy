2564ec7e977821d1b1e9bfba0731e4e24a955420
josh <jalexy12@gmail.com>
2015-07-18 15:51:19 -0400

    Added omniauth callback controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -18,11 +18,11 @@ class IngredientsController < ApplicationController
   end
 
   def create
-    responder("Ingredient was successfully created")
+    responder("Ingredient was successfully created", @ingredient)
   end
 
   def update
-    responder("Ingredient was successfully updated")
+    responder("Ingredient was successfully updated", @ingredient)
   end
 
   def destroy
@@ -35,27 +35,27 @@ class IngredientsController < ApplicationController
 
   private
 
-    def new_ingredient
-      @ingredient = Ingredient.new(ingredient_params)
-    end
+  def new_ingredient
+    @ingredient = Ingredient.new(ingredient_params)
+  end
 
-    def set_ingredient
-      @ingredient = Ingredient.find(params[:id])
-    end
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
 
-    def responder(notice)
-      respond_to do |format|
-        if @ingredient.save
-          format.html { redirect_to @ingredient, notice: notice }
-          format.json { render :show, status: :created, location: @ingredient }
-        else
-          format.html { render :new }
-          format.json { render json: @ingredient.errors, status: :unprocessable_entity }
-        end
+  def responder(notice, ingredient)
+    respond_to do |format|
+      if @ingredient.save
+        format.html { redirect_to ingredient, notice: notice }
+        format.json { render :show, status: :created, location: ingredient }
+      else
+        format.html { render :new }
+        format.json { render json: ingredient.errors, status: :unprocessable_entity }
       end
     end
+  end
 
-    def ingredient_params
-      params.require(:ingredient).permit(:name)
-    end
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
 end
