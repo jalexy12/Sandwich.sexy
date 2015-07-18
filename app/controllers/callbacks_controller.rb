defa23b41a16c6008bd32fa2f4892b57a87b57f2
josh <jalexy12@gmail.com>
2015-07-18 16:28:33 -0400

    Finished omniauth

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,5 +1,7 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def instagram
+    	ap "Controller"
+    	ap request.env['omniauth.auth']
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
     end
